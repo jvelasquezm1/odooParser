a37c28bf38ea46353df7ee2eb4806e650d337e8a
Juan Velasquez <jvelasquez@unac.edu.co>
2022-11-30 15:50:21 +0100

    Update equal comparison

--- parser.ts
+++ parser.ts
@@ -15,8 +15,16 @@ enum Operators {
   LessOrEqualThan = "<=",
 }
 
+const setValueForEqualComparison = (expression: string) =>
+  expression
+    .replace("%", "")
+    .replace(/\[|\]/g, "")
+    .replace(")", "")
+    .replace(/'/g, "")
+    .trim();
+
 const setValueForComparison = (expression: string) =>
-  expression.replace("%", "").replace(")", "").replace(/'/g, "");
+  setValueForEqualComparison(expression).replace(/\s/g, "").toLocaleLowerCase();
 
 const setArrayForComparison = (expression: string[]) => {
   const regExpBrackets = /\[([^)]+)\]/g;
@@ -26,52 +34,54 @@ const setArrayForComparison = (expression: string[]) => {
 
 export const parseOdooDomain = (
   odooDomain: string,
-  account: { [key: string]: string }
+  account: { [key: string]: any }
 ) => {
   const regExpParenthesis = /\(([^)]+)\)/g;
   const odooDomainCleaned = odooDomain
     .replace(regExpParenthesis, "condition")
     .replace(/\[|\]/g, "")
-    .replace(/\s/g, "")
     .split(",");
-  const comparisons =
-    odooDomain.replace(/\s/g, "").match(regExpParenthesis) || [];
+  const comparisons = odooDomain.match(regExpParenthesis) || [];
   let comparisonCounter = 0;
   let result;
 
   const conditions = odooDomainCleaned.map((condition) => {
-    if (["|", "&"].includes(condition.replace(/'/g, ""))) {
+    if (["|", "&"].includes(condition.replace(/'/g, "").trim())) {
       return condition;
     }
+    if (!comparisons[comparisonCounter]) return;
     const expression = comparisons[comparisonCounter].split(",") || [];
     comparisonCounter++;
     const operator = expression[1].replace(/'/g, "");
-    const fieldName = expression[0]
-      .replace("(", "")
-      .replace(/'/g, "")
-      .split(".")[1];
-    switch (operator) {
+    const field = expression[0].replace("(", "").replace(/'/g, "").split(".");
+    const fieldName = field[1];
+    if (!account[fieldName]) return false;
+    let accountFieldName = account[fieldName];
+    if (account[fieldName] instanceof Array) {
+      accountFieldName = account[fieldName][field[2] ? 1 : 0];
+    }
+    switch (operator.replace(/\s/g, "")) {
       case Operators.EqualLike:
         return (
-          account[fieldName].toLowerCase() ===
+          accountFieldName.toString().replace(/\s/g, "").toLowerCase() ===
           setValueForComparison(expression[2])
         );
       case Operators.NotLike:
         return (
-          account[fieldName].toLowerCase() !==
+          accountFieldName.toString().replace(/\s/g, "").toLowerCase() !==
           setValueForComparison(expression[2])
         );
       case Operators.Equal:
         return (
-          account[fieldName].toLowerCase() ===
-          setValueForComparison(expression[2])
+          accountFieldName.toString() ===
+          setValueForEqualComparison(expression[2])
         );
       case Operators.In:
         return setArrayForComparison(expression)?.includes(
-          account[fieldName].toString()
+          accountFieldName.toString()
         );
       default:
-        return undefined;
+        return;
     }
   });
 
@@ -79,7 +89,10 @@ export const parseOdooDomain = (
     if (i === conditions.length) {
       result = conditions[i - 1];
     } else {
-      if (conditions[i - 2] === "'|'" || conditions[i - 2] === "'&'") {
+      if (
+        `${conditions[i - 2]}`.trim() === "'|'" ||
+        `${conditions[i - 2]}`.trim() === "'&'"
+      ) {
         result =
           conditions[i - 2] === "'&'"
             ? result && conditions[i - 1]
@@ -92,9 +105,10 @@ export const parseOdooDomain = (
 };
 
 const odooDomain =
-  "['|',('account_id.code', '=like', '454%'), '&', ('account_id.code', '=like', '455%'), '|', ('account_id.code', '=like', '456%'), '|', ('account_id.code', '=like', '457%'), '|', ('account_id.code', '=like', '458%'), ('account_id.code', '=like', '459%')]";
-//   "['&', ('account_id.user_type_id', 'in', [3, 5, 7]), '&', ('account_id.user_type_id', 'in', [3]), ('account_id.user_type_id.type', '=', 'receivable')]";
-// "[('account_id.code', '=like', '695%')]";
+  // "['|',('account_id.code', '=like', '454%'), '&', ('account_id.code', '=like', '455%'), '|', ('account_id.code', '=like', '456%'), '|', ('account_id.code', '=like', '457%'), '|', ('account_id.code', '=like', '458%'), ('account_id.code', '=like', '459%')]"
+  //   "['&', ('account_id.user_type_id', 'in', [3, 5, 7]), '&', ('account_id.user_type_id', 'in', [3]), ('account_id.user_type_id.type', '=', 'receivable')]"
+  // "[('account_id.code', '=like', '695%')]"
+  "['&', ('account_id.user_type_id', 'in', [9, 4]), '|', ('account_id.user_type_id.type', '=', 'Fixed Assets'), ('account_id.non_trade', '=', True)]";
 
-const account = { code: "454", user_type_id: "3" };
+const account = { code: "454", user_type_id: [9, "Fixed Assets"] };
 console.log(parseOdooDomain(odooDomain, account));
