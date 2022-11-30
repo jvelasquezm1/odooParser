423dbce017bdaba88cc9672984e9ca81a1035f34
Juan Velasquez <jvelasquez@unac.edu.co>
2022-11-30 13:52:52 +0100

    Set function

--- parser.ts
+++ parser.ts
@@ -1,24 +1,23 @@
-const test =
-  "['&',('account_id.code', '=like', '454%'), '&', ('account_id.code', '=like', '455%'), '|', ('account_id.code', '=like', '456%'), '|', ('account_id.code', '=like', '457%'), '|', ('account_id.code', '=like', '458%'), ('account_id.code', '=like', '459%')]";
-//   "['&', ('account_id.user_type_id', 'in', [3, 5, 7]), '&', ('account_id.user_type_id', 'in', [3]), ('account_id.user_type_id.type', '=', 'receivable')]";
+const parseOdooDomain = (
+  odooDomain: string,
+  account: { [key: string]: string }
+) => {
+  const regExpParenthesis = /\(([^)]+)\)/g;
+  const regExpBrackets = /\[([^)]+)\]/g;
+  const expressions = odooDomain
+    .replace(regExpParenthesis, "condition")
+    .replace(/\[|\]/g, "")
+    .replace(/\s/g, "")
+    .split(",");
+  const comparisons =
+    odooDomain.replace(/\s/g, "").match(regExpParenthesis) || [];
+  let comparisonCounter = 0;
+  let result;
 
-const testAccount = { code: "454", user_type_id: "3" };
-
-const regExpParenthesis = /\(([^)]+)\)/g;
-const regExpBrackets = /\[([^)]+)\]/g;
-const expressions = test
-  .replace(regExpParenthesis, "condition")
-  .replace(/\[|\]/g, "")
-  .replace(/\s/g, "")
-  .split(",");
-const comparisons = test.replace(/\s/g, "").match(regExpParenthesis) || [];
-let comparisonCounter = 0;
-let temp;
-
-const conditions = expressions.map((condition) => {
-  if (condition.startsWith("'|'") || condition.startsWith("'&'")) {
-    return condition;
-  } else {
+  const conditions = expressions.map((condition) => {
+    if (condition.startsWith("'|'") || condition.startsWith("'&'")) {
+      return condition;
+    }
     const expression = comparisons[comparisonCounter].split(",") || [];
     comparisonCounter++;
     const operator = expression[1].replace(/'/g, "");
@@ -31,32 +30,41 @@ const conditions = expressions.map((condition) => {
         .replace("%", "")
         .replace(")", "")
         .replace(/'/g, "");
-      return testAccount[fieldName].toLowerCase() === value.toLowerCase();
+      return account[fieldName].toLowerCase() === value.toLowerCase();
     }
     if (operator === "=") {
       const value = expression[2].replace(")", "").replace(/'/g, "");
-      return testAccount[fieldName] === value;
+      return account[fieldName] === value;
     }
     if (operator === "in") {
       const values = expression?.join(",")?.match(regExpBrackets);
       const valuesCleaned = values
         ? values[0].replace("[", "").replace("]", "").split(",")
         : "";
-      return valuesCleaned.includes(testAccount[fieldName].toString());
+      return valuesCleaned.includes(account[fieldName].toString());
     }
-  }
-});
+    return undefined;
+  });
 
-for (let i = conditions.length; i > 0; i--) {
-  if (i === conditions.length) {
-    temp = conditions[i - 1];
-  } else {
-    if (conditions[i - 2] === "'|'" || conditions[i - 2] === "'&'") {
-      temp =
-        conditions[i - 2] === "'&'"
-          ? temp && conditions[i - 1]
-          : temp || conditions[i - 1];
-      conditions.splice(i - 2, 1);
+  for (let i = conditions.length; i > 0; i--) {
+    if (i === conditions.length) {
+      result = conditions[i - 1];
+    } else {
+      if (conditions[i - 2] === "'|'" || conditions[i - 2] === "'&'") {
+        result =
+          conditions[i - 2] === "'&'"
+            ? result && conditions[i - 1]
+            : result || conditions[i - 1];
+        conditions.splice(i - 2, 1);
+      }
     }
   }
-}
+  return result;
+};
+
+const odooDomain =
+  "['|',('account_id.code', '=like', '454%'), '&', ('account_id.code', '=like', '455%'), '|', ('account_id.code', '=like', '456%'), '|', ('account_id.code', '=like', '457%'), '|', ('account_id.code', '=like', '458%'), ('account_id.code', '=like', '459%')]";
+//   "['&', ('account_id.user_type_id', 'in', [3, 5, 7]), '&', ('account_id.user_type_id', 'in', [3]), ('account_id.user_type_id.type', '=', 'receivable')]";
+
+const account = { code: "454", user_type_id: "3" };
+console.log(parseOdooDomain(odooDomain, account));
